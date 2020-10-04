8bf5f9774d8cc911d7ba91e6761285a3867d5996
Felipe Rocha <felipemota.rocha@gmail.com>
2020-10-03 23:29:18 -0300

    feat [server]: try catch blocks, some code improvements (now in stocks resolver)

--- server/src/graphql/resolvers/stock.resolvers.ts
+++ server/src/graphql/resolvers/stock.resolvers.ts
@@ -1,3 +1,4 @@
+import { IStock } from './../../models/stock.model';
 import {
 	Resolver,
 	Query,
@@ -9,25 +10,38 @@ import {
 import Stock from '../../models/stock.model';
 import User from '../../models/user.model';
 import { default as StockType } from '../types/stock.types';
+import { ApolloError } from 'apollo-server-express';
 
 @Resolver((_of) => StockType)
 class StockResolver {
 	@Query(() => [StockType])
 	async stocks() {
-		const stocks = await Stock.find({});
-		return stocks;
+		try {
+			const stocks = await Stock.find({});
+			return stocks;
+		} catch (_err) {
+			return new ApolloError('Something went wrong.');
+		}
 	}
 
 	@Query(() => StockType)
 	async stock(@Arg('symbol') symbol: string) {
-		const stock = await Stock.findOne({ symbol });
-		return stock;
+		try {
+			const stock = await Stock.findOne({ symbol });
+			return stock;
+		} catch (_err) {
+			return new ApolloError('Something went wrong.');
+		}
 	}
 
 	@FieldResolver()
-	async buyers(@Root() stock: any) {
-		const users = await User.find({ 'stocks.stockId': stock._doc._id });
-		return users;
+	async buyers(@Root() stock: IStock) {
+		try {
+			const users = await User.find({ 'stocks.stockId': stock._id });
+			return users;
+		} catch (_err) {
+			return new ApolloError('Something went wrong.');
+		}
 	}
 
 	@Mutation(() => StockType)
@@ -36,14 +50,18 @@ class StockResolver {
 		@Arg('symbol') symbol: string,
 		@Arg('price') price: number
 	) {
-		const stock = new Stock({
-			companyName,
-			symbol,
-			price,
-		});
-		await stock.save();
+		try {
+			const stock = new Stock({
+				companyName,
+				symbol,
+				price,
+			});
+			await stock.save();
 
-		return stock;
+			return stock;
+		} catch (_err) {
+			return new ApolloError('Something went wrong.');
+		}
 	}
 }
 
