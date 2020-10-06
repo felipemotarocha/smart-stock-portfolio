4be0a81b63b16c29dc5c1fafc3ba10d995ff6ff0
Felipe Rocha <felipemota.rocha@gmail.com>
2020-10-06 18:37:45 -0300

    feat [client]: handling change on balance info component

--- client/src/components/balance-info/balance-info.component.tsx
+++ client/src/components/balance-info/balance-info.component.tsx
@@ -1,28 +1,48 @@
 import * as React from 'react';
+import { useState } from 'react';
 
 import { Container, InputContainer, Headline } from './balance-info.styles';
 import CustomInput from '../custom-input/custom-input.component';
 
 export interface BalanceInfoProps {}
 
-const BalanceInfo: React.FunctionComponent<BalanceInfoProps> = (props) => {
+const BalanceInfo: React.FunctionComponent<BalanceInfoProps> = () => {
+	const [availableBalance, setAvailableBalance] = useState<number>(0);
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const re = /^[0-9\b]+$/;
+
+		if (e.target.value === '') {
+			setAvailableBalance(0);
+		} else if (re.test(e.target.value)) {
+			setAvailableBalance(parseInt(e.target.value));
+		}
+	};
+
 	return (
 		<Container>
 			<InputContainer>
 				<Headline>Available balance</Headline>
-				<CustomInput width={30} prefix='R$' type='text' size='large' />
+				<CustomInput
+					width={30}
+					prefix='R$'
+					type='text'
+					size='large'
+					onChange={handleChange}
+					value={availableBalance}
+				/>
 			</InputContainer>
 
 			<InputContainer>
 				<Headline>Invested balance</Headline>
 				<CustomInput
-					width={30}
-					disabled
-					prefix='R$'
 					type='text'
+					width={30}
+					prefix='R$'
 					size='large'
 					backgroundColor='#1488cc'
 					borderColor='#1488cc'
+					readOnly
 				/>
 			</InputContainer>
 
@@ -30,12 +50,12 @@ const BalanceInfo: React.FunctionComponent<BalanceInfoProps> = (props) => {
 				<Headline>Total balance</Headline>
 				<CustomInput
 					width={30}
-					disabled
 					prefix='R$'
 					type='text'
 					size='large'
 					backgroundColor='#1488cc'
 					borderColor='#1488cc'
+					readOnly
 				/>
 			</InputContainer>
 		</Container>
