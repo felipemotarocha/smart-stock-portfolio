18f310d686ae1bc50b7441e700b40ac394729eb4
Felipe Rocha <felipemota.rocha@gmail.com>
2020-10-08 02:07:58 -0300

    feat [client]: style improvements

--- client/src/components/stock-status/stock-status.styles.tsx
+++ client/src/components/stock-status/stock-status.styles.tsx
@@ -20,7 +20,9 @@ export const Container = styled.div`
 export const Status = styled.div<StatusProps>`
 	background-color: ${({ status }) =>
 		status === 'Wait' ? '#e6494c' : '#4bc05e'};
-	padding: 0.2rem 1.4rem;
+	padding: 0.2rem 0;
+	width: 90px;
+	text-align: center;
 	border-radius: 5px;
 	margin-top: 0.1rem;
 
